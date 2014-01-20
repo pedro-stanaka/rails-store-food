04fb82163865c3abf531f2a94886686ded676bd6
Pedro Tanaka <pedro.tanaka@sinetic.com.br>
2014-01-20 14:14:52 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CantinhoDoAlergico::Application.config.secret_token = 'b8cf14c4f4a778fe7daa9a310454daaa36ffbb014cf673e6d954dc3137d0e0afa04c64ff75b516b7fe97da1cc93b89a3225c8b3e0cb34aab7ed2c2d96fc39343'
